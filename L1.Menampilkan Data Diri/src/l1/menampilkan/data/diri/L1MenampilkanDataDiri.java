a68890b3d97e3ae89e7a44f885da1031ef9b129e
jamaludin19 <jamaludinma321@gmail.com>
2019-11-14 11:58:25 +0700

    Tugas-Java

---
+++ L1.Menampilkan Data Diri/src/l1/menampilkan/data/diri/L1MenampilkanDataDiri.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package l1.menampilkan.data.diri;
+
+/**
+ * NAMA  : Jamaludin Muhamad Akbar
+ * NIM   : A2.1900090
+ * Kelas : TI-1A
+ * 
+ * @author JamalaudinMuhamadAkbar
+ */
+public class L1MenampilkanDataDiri {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Biodata" + "\n" + "-----------------" );
+        System.out.println("NAMA\t\t         : Jamaludin Muhamad Akbar");
+        System.out.println("ALAMAT\t\t         : Dsn.Cimanglid Ds.Sukamaju Kec.Rancakalong Kab.Sumedang ");
+        System.out.println("TEMPAT LAHIR\t\t : Sumedang ");
+        System.out.println("TANGGAL LAHIR\t\t : 03 Januari 2000");
+        System.out.println("--------------------------");
+        System.out.println("HOBI\t\t: Futsal");
+        System.out.println("MAKES\t\t: Nasi Goreng");
+        System.out.println("MIKES\t\t: Susu");
+    }
+    
+}
---
+++ L6.KambingStatic/src/l6/kambingstatic/claskambing.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package l6.kambingstatic;
+
+/**
+ *
+ * @author E5-411-CG5D
+ */
+public class claskambing {
+    class Mamalia  {
+    // Variabel jumlahKambing dideklarasikan sebagai statik
+    public static int jumlahKambing;
+
+}
+}
